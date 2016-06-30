5754671dc80a0d8e596da19e9810cb15a25ac323
Wahid Alimi <WahidAlimi@ads.deltek.com>
2016-06-30 10:56:51 -0400

    added .js and .css files to breakout .html file

---
+++ v2/template_designer.js
@@ -0,0 +1,18 @@
+$(function () {
+    var options = {
+        float: true
+    };
+    $('.grid-stack').gridstack(options);
+    new function () {
+        this.grid = $('.grid-stack').data('gridstack');
+        this.addNewWidget = function () {
+            this.grid.addWidget($('<div><div class="grid-stack-item-content"><input type="text" class="form-control"></div><div/>'), 0, 0, 2, 1);
+        	return false;
+        }.bind(this);
+        
+       	$('#add-new-header').click(this.addNewWidget);
+
+    };
+
+    
+});
