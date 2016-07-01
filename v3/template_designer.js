c0cecf4e37a238171d50216e54332afa25f7470b
Wahid Alimi <WahidAlimi@ads.deltek.com>
2016-07-01 08:33:56 -0400

    separated v3 template_designer into .js and .css files. made minor css changes.

---
+++ v3/template_designer.js
@@ -0,0 +1,18 @@
+$(function() {
+  $("#draggable-header").draggable({
+    helper: 'clone',
+    cursor: 'move'
+  });
+
+  $( "#droppable" ).droppable({
+    tolerance: 'fit',
+    drop: function(event, ui){
+          $("<div></div>")
+          .html("<input type='text' id='header' class='form-control'>")
+          .appendTo($(this));
+    }
+  });
+
+
+
+});
