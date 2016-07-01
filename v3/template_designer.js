2005e83267e5ee4c11feeeee48be54835807cc02
Wahid Alimi <WahidAlimi@ads.deltek.com>
2016-07-01 15:16:08 -0400

    added image placeholder. added functionality to show/hide properties windows based on the component type. small css updates.

--- v3/template_designer.js
+++ v3/template_designer.js
@@ -1,4 +1,6 @@
 $(function () {
+  $("#heading-properties").hide();
+  $("#image-properties").hide();
   $(".component-item").draggable({
     helper: 'clone',
     cursor: 'move'
@@ -13,23 +15,29 @@ $(function () {
         .html(htmlInput(componentType))
         .appendTo($(this));
 
-      $('#component-heading').blur(function () {
-        $(this).addClass("input-no-border");
+      $('.component-heading')
+      .blur(function () {
+        $(this).addClass("input-no-border")
+      })
+      .focus(function() {
+        $("#heading-properties").show();
       });
     }
   });
-
-
 
   var htmlInput = function (componentType) {
 
     switch (componentType) {
       case 'draggable-heading':
+        $("#heading-properties").show();
+        $("#image-properties").hide();
         return "<input type='text' id='component-heading' class='component-heading'>";
       case 'draggable-text':
         return "<input type='text' id='component-text' class='form-control'>";
       case 'draggable-image':
-        return "<input type='file' id='myFile' size='50'>";
+        $("#heading-properties").hide();
+        $("#image-properties").show();
+        return "<div class='component-image'></div>";
       case 'draggable-table':
         return "<table class='component-table'><tr><td><input type='text' class='form-control'></td><td><input type='text' class='form-control'></td></tr></table>";
       case 'draggable-pricing-table':
