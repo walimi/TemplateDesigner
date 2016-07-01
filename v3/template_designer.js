ab500f6f6386bc6ea665a2ad9b07705fe6b43933
Wahid Alimi <WahidAlimi@ads.deltek.com>
2016-07-01 11:39:29 -0400

    added dynamic functionality

--- v3/template_designer.js
+++ v3/template_designer.js
@@ -4,15 +4,40 @@ $(function() {
     cursor: 'move'
   });
 
-  $( "#droppable" ).droppable({
+  $("#droppable").droppable({
     tolerance: 'fit',
     drop: function(event, ui){
+          var componentType = ui.draggable.attr("id");
+   
           $("<div></div>")
-          .html("<input type='text' id='header' class='form-control'>")
+          .html(htmlInput(componentType))
           .appendTo($(this));
     }
   });
 
+  var htmlInput = function(componentType) {
+  	
+  	switch(componentType) {
+	    case 'draggable-heading':
+	        return "<input type='text' id='component-heading' class='form-control'>";
+	        break;
+        case 'draggable-text':
+	        return "<input type='text' id='component-text' class='form-control'>";
+	        break;
+	    case 'draggable-image':
+	        return "<input type='file' id='myFile' size='50'>";
+	        break;
+        case 'draggable-table':
+        	return "<table class='component-table'><tr><td><input type='text' class='form-control'></td><td><input type='text' class='form-control'></td></tr></table>";
+        	break;
+    	case 'draggable-pricing-table':
+        	return "<table class='component-table'><tr><td><input type='text' class='form-control'></td><td><input type='text' class='form-control'></td></tr></table>";
+        	break;
+	    default:
+	        return "<input type='text' class='form-control'>";
+	}
+  }
 
-
+  var focusedElem = $(':focus');
+  console.log(focusedElem);
 });
