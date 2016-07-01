3c7bd6a10e7b53c0ea42e6369b1bf747e227eb15
Wahid Alimi <WahidAlimi@ads.deltek.com>
2016-07-01 12:43:51 -0400

    additional changes to css

--- v3/template_designer.js
+++ v3/template_designer.js
@@ -1,4 +1,4 @@
-$(function() {
+$(function () {
   $(".component-item").draggable({
     helper: 'clone',
     cursor: 'move'
@@ -6,38 +6,39 @@ $(function() {
 
   $("#droppable").droppable({
     tolerance: 'fit',
-    drop: function(event, ui){
-          var componentType = ui.draggable.attr("id");
-   
-          $("<div></div>")
-          .html(htmlInput(componentType))
-          .appendTo($(this));
+    drop: function (event, ui) {
+      var componentType = ui.draggable.attr("id");
+
+      $("<div></div>")
+        .html(htmlInput(componentType))
+        .appendTo($(this));
+
+      $('#component-heading').blur(function () {
+        $(this).addClass("input-no-border");
+      });
     }
   });
 
-  var htmlInput = function(componentType) {
-  	
-  	switch(componentType) {
-	    case 'draggable-heading':
-	        return "<input type='text' id='component-heading' class='form-control'>";
-	        break;
-        case 'draggable-text':
-	        return "<input type='text' id='component-text' class='form-control'>";
-	        break;
-	    case 'draggable-image':
-	        return "<input type='file' id='myFile' size='50'>";
-	        break;
-        case 'draggable-table':
-        	return "<table class='component-table'><tr><td><input type='text' class='form-control'></td><td><input type='text' class='form-control'></td></tr></table>";
-        	break;
-    	case 'draggable-pricing-table':
-        	return "<table class='component-table'><tr><td><input type='text' class='form-control'></td><td><input type='text' class='form-control'></td></tr></table>";
-        	break;
-	    default:
-	        return "<input type='text' class='form-control'>";
-	}
+
+
+  var htmlInput = function (componentType) {
+
+    switch (componentType) {
+      case 'draggable-heading':
+        return "<input type='text' id='component-heading' class='component-heading'>";
+      case 'draggable-text':
+        return "<input type='text' id='component-text' class='form-control'>";
+      case 'draggable-image':
+        return "<input type='file' id='myFile' size='50'>";
+      case 'draggable-table':
+        return "<table class='component-table'><tr><td><input type='text' class='form-control'></td><td><input type='text' class='form-control'></td></tr></table>";
+      case 'draggable-pricing-table':
+        return "<table class='component-table'><tr><td><input type='text' class='form-control'></td><td><input type='text' class='form-control'></td></tr></table>";
+      default:
+        return "<input type='text' class='form-control'>";
+    }
   }
 
-  var focusedElem = $(':focus');
-  console.log(focusedElem);
+
+
 });
