fd5673fca98c861434896c67e06d249d09bda9ce
Wahid Alimi <WahidAlimi@ads.deltek.com>
2016-07-01 16:06:56 -0400

    introduced font-awesome. minor refactorings in js. minor updates to css.

--- v3/template_designer.js
+++ v3/template_designer.js
@@ -1,6 +1,8 @@
 $(function () {
   $("#heading-properties").hide();
   $("#image-properties").hide();
+
+
   $(".component-item").draggable({
     helper: 'clone',
     cursor: 'move'
@@ -12,21 +14,17 @@ $(function () {
       var componentType = ui.draggable.attr("id");
 
       $("<div></div>")
-        .html(htmlInput(componentType))
+        .html(componentCreated(componentType))
         .appendTo($(this));
 
       $('.component-heading')
-      .blur(function () {
-        $(this).addClass("input-no-border")
-      })
-      .focus(function() {
-        $("#heading-properties").show();
-      });
+        .blur(function () {
+          $(this).addClass("input-no-border");
+        });
     }
   });
 
-  var htmlInput = function (componentType) {
-
+  var componentCreated = function (componentType) {
     switch (componentType) {
       case 'draggable-heading':
         $("#heading-properties").show();
@@ -37,7 +35,7 @@ $(function () {
       case 'draggable-image':
         $("#heading-properties").hide();
         $("#image-properties").show();
-        return "<div class='component-image'></div>";
+        return "<div class='component-image' id='component-image'></div>";
       case 'draggable-table':
         return "<table class='component-table'><tr><td><input type='text' class='form-control'></td><td><input type='text' class='form-control'></td></tr></table>";
       case 'draggable-pricing-table':
@@ -47,6 +45,15 @@ $(function () {
     }
   }
 
+  $("#heading-font").on("change", function () {
+    var selectedFontFamily = $('#heading-font option:selected').text();
+    console.log("heading font changed to " + selectedFontFamily);
+  });
 
+  $("#component-image").click(function () {
+    console.log("component image was clicked");
+    $("#heading-properties").hide();
+    $("#image-properties").show();
+  });
 
 });
